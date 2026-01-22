beb3b80faa2073fa431f94adc0df14b9011adb00
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2026-01-22 19:13:38 +1000

    cleaning out copilot crap

--- config.lua
+++ config.lua
@@ -374,16 +374,16 @@ lvim.plugins = {
 --   return
 -- end
 
-copilot.setup {
-  suggestion = {
-    keymap = {
-      accept = "<c-l>",
-      next = "<c-j>",
-      prev = "<c-k>",
-      dismiss = "<c-h>",
-    },
-  },
-}
+-- copilot.setup {
+--   suggestion = {
+--     keymap = {
+--       accept = "<c-l>",
+--       next = "<c-j>",
+--       prev = "<c-k>",
+--       dismiss = "<c-h>",
+--     },
+--   },
+-- }
 
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<c-a>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
